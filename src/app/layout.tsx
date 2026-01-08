51cffdc9e5c020e5aa7bf34edfa75f175a7c511b
Thulio Costa <ThulioCosta8@gmail.com>
2026-01-08 12:14:37 +0000

    O shortcut icon deve ser o emoji de coração vermelho

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
   title: 'It Takes Two',
   description: 'Seu espaço para cultivar o amor e a parceria.',
   icons: {
-    icon: '/favicon.svg',
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>❤️</text></svg>",
   }
 };
 
